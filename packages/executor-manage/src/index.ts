1c60f9997ce4cb322553525530e7fa41f6801baa
Ondrej Rafaj <dictator@dictator.lan>
2026-08-12 00:35:59 +0100

    feat: add executor availability authority

---
+++ packages/executor-manage/src/index.ts
@@ -0,0 +1,10 @@
+export {
+  canManagePrivateAssignments,
+  resolveExecutorAvailability,
+  type ExecutorAvailabilityDecision,
+  type ExecutorAvailabilityInput,
+  type OrganizationScopeAvailability,
+  type PrivateScopeAvailability,
+  type ProjectScopeAvailability,
+  type ScopeAvailability,
+} from './availability.js'
