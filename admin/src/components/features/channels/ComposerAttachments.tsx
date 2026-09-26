9a406a7550ce3d85d6fd19aa28c61a6df100f613
Ondrej Rafaj <dictator@dictator.lan>
2026-09-26 21:50:44 +0100

    fix channel attachment localization import

--- admin/src/components/features/channels/ComposerAttachments.tsx
+++ admin/src/components/features/channels/ComposerAttachments.tsx
@@ -1,4 +1,5 @@
 import { faXmark } from '@fortawesome/free-solid-svg-icons'
+import { useTranslation } from 'react-i18next'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { formatBytes } from '../../../lib/upload-xhr'
 import type { ComposerAttachments as ComposerAttachmentsState } from './useComposerAttachments'
